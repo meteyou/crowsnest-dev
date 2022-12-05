497d9fa36389c01daa9b2ff7aae007b7d53f10a0
Stephan Wendel <me@stephanwe.de>
2022-12-05 22:01:04 +0100

    fix: fix error values not set properly
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/v4l2_control.sh
+++ libs/v4l2_control.sh
@@ -16,12 +16,12 @@
 # shellcheck enable=require-variable-braces
 
 # Exit upon Errors
-set -eE
+set -Ee
 
-function v4l2_control {
+v4l2_control() {
     log_msg "V4L2 Control:"
 
-    function main {
+    main() {
         local device v4l2ctl valueless opt_avail
         for cam in $(configured_cams); do
             # get device from cam section
@@ -29,7 +29,7 @@ function v4l2_control {
             # get v4l2ctl parameters
             v4l2ctl="$(get_param "cam ${cam}" v4l2ctl)"
             # if not empty do
-            if [ -n "${v4l2ctl}" ]; then
+            if [[ -n "${v4l2ctl}" ]]; then
                 # Write configured options to Log
                 log_msg "Device: [cam ${cam}]"
                 log_msg "Options: ${v4l2ctl}"
@@ -42,13 +42,15 @@ function v4l2_control {
                     valueless="$(echo "${param}" | cut -d "=" -f1)"
                     opt_avail="$(v4l2-ctl -d "${device}" -L | \
                     grep -c "${valueless}" || true)"
-                    if [ "${opt_avail}" -eq "0" ]; then
+                    if [[ "${opt_avail}" -eq "0" ]]; then
                         log_msg "Parameter '${param}' not available for '${device}'. Skipped."
                     else
                         v4l2-ctl -d "${device}" -c "${param}" 2> /dev/null
                     fi
+                    # Maybe some devices take some time, therefor "pause" a bit
+                    sleep 0.1
                 done
-                    if [ "$(log_level)" == "debug" ]; then
+                    if [[ "$(log_level)" == "debug" ]]; then
                         v4l2-ctl -d "${device}" -L | log_output "v4l2ctl"
                     fi
             else
@@ -61,11 +63,11 @@ function v4l2_control {
 main
 }
 
-function brokenfocus {
+brokenfocus() {
     # checks if "focus_absolute" is configured
     # call if_focus_absolute <mycamnameornumber>
     # returns 1 = true, 0 = false
-    function if_focus_absolute {
+    if_focus_absolute() {
         local cam
         cam="${1}"
         get_param "cam ${cam}" v4l2ctl | grep -c "focus_absolute"
@@ -74,19 +76,19 @@ function brokenfocus {
     # checks if device has "focus_absolute"
     # call has_focus_absolute <device>
     # returns 1 if true, 0 if false
-    function has_focus_absolute {
+    has_focus_absolute() {
         v4l2-ctl -d "${1}" -C "focus_absolute" &> /dev/null \
         && echo "1" || echo "0"
     }
 
     # call get_conf_value <mycamnameornumber>
     # spits out value from config file
-    function get_conf_value {
+    get_conf_value() {
         local cam conf_val
         local -a params
         cam="${1}"
         conf_val="$(get_param "cam ${cam}" v4l2ctl)"
-        if [ -n "${conf_val}" ]; then
+        if [[ -n "${conf_val}" ]]; then
             IFS=','; read -ra params <<< "${conf_val}"
             unset IFS
             for i in "${params[@]}"; do
@@ -98,32 +100,32 @@ function brokenfocus {
     # call get_current_value <device>
     # ex.: get_current_value /dev/video0
     # spits out focus_absolute=20 ( if set to 20 )
-    function get_current_value {
+    get_current_value() {
         v4l2-ctl -d "${1}" -C "focus_absolute" 2> /dev/null | sed 's/:[[:space:]]/=/'
     }
 
     # call set_current_value <device> <value>
     # ex.: set_current_value /dev/video0 focus_absolute=30
-    function set_focus_absolute {
+    set_focus_absolute() {
         local device value
         device="${1}"
         value="${2}"
         v4l2-ctl -d "${device}" -c "${value}"
     }
 
-    function main {
+    main() {
         local cur_val conf_val device
         for cam in $(configured_cams); do
             device="$(get_param "cam ${cam}" device)"
             cur_val="$(get_current_value "${device}")"
             conf_val="$(get_conf_value "${cam}")"
-            if [ "$(has_focus_absolute "${device}")" == "1" ] &&
-            [ "$(if_focus_absolute "${cam}")" == "1" ] &&
-            [ "${cur_val}" != "${conf_val}" ]; then
+            if [[ "$(has_focus_absolute "${device}")" == "1" ]] &&
+            [[ "$(if_focus_absolute "${cam}")" == "1" ]] &&
+            [[ "${cur_val}" != "${conf_val}" ]]; then
                 detected_broken_dev_msg
                 set_focus_absolute "${device}" "${conf_val}"
             fi
-            if [ "$(log_level)" == "debug" ] && [ -n "${cur_val}" ]; then
+            if [[ "$(log_level)" == "debug" ]] && [[ -n "${cur_val}" ]]; then
                 debug_focus_val_msg "$(get_current_value "${device}")"
             fi
         done
@@ -139,11 +141,11 @@ main
 # a "block-like" view after reboots
 # To prevent that blockyfix should apply constant bitrate befor start of ustreamer
 # See https://github.com/mainsail-crew/crowsnest/issues/33
-function blockyfix {
+blockyfix() {
     local dev v4l2ctl
 
     # call set_bitrate <device>
-    function set_bitrate {
+    set_bitrate() {
         v4l2-ctl -d "${1}" -c video_bitrate_mode=1 2> /dev/null
         v4l2-ctl -d "${1}" -c video_bitrate=15000000 2> /dev/null
     }
@@ -151,9 +153,9 @@ function blockyfix {
     for cam in $(configured_cams); do
         dev="$(get_param "cam ${cam}" device)"
         v4l2ctl="$(get_param "cam ${cam}" v4l2ctl)"
-        if [ "${dev}" = "$(dev_is_raspicam)" ]; then
-            if [ -z "${v4l2ctl}" ] ||
-            [ "$(grep -c "video_bitrate" <<< "${v4l2ctl}")" == "0" ]; then
+        if [[ "${dev}" = "$(dev_is_raspicam)" ]]; then
+            if [[ -z "${v4l2ctl}" ]] ||
+            [[ "$(grep -c "video_bitrate" <<< "${v4l2ctl}")" == "0" ]]; then
                 set_bitrate "${dev}"
                 blockyfix_msg_1
             fi
